533cb6379260d68eb330a20d5b09476f43b9524f
duyilemi <67197664+duyilemi@users.noreply.github.com>
2022-03-20 22:17:45 +0100

    secret

--- components/Feed.js
+++ components/Feed.js
@@ -36,8 +36,6 @@ const Feed = () => {
     [db]
   )
 
-  console.log(posts)
-
   return (
     <div className="max-w-2xl flex-grow border-l border-r border-gray-700 sm:ml-[73px] xl:ml-[370px]">
       <div className="sticky top-0 z-50 flex items-center border-b border-gray-700 bg-black py-2 px-3 text-[#d9d9d9] sm:justify-between">
--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,6 @@ const Home = ({ trendingResults, followResults, providers }) => {
 
         {/* Feed */}
         <Feed />
-        {console.log(session)}
 
         {/* Modal */}
         {isOpen && <Modal />}
